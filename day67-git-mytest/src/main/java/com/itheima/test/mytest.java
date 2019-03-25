56814650eaa32ebfba6d5a72d10d4f3b0404b5a8
weihong <lwh3973@163.com>
2019-03-25 20:56:06 +0800

    第一次修改

--- day67-git-mytest/src/main/java/com/itheima/test/mytest.java
+++ day67-git-mytest/src/main/java/com/itheima/test/mytest.java
@@ -5,5 +5,6 @@ public class mytest {
     public static void main(String[] args) {
         System.out.println("============");
         System.out.println("====1111111===");
+		 System.out.println("====2222===");
     }
 }
